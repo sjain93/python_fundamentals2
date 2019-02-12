f8b13dede15ef7bc32d37579f8f732eb39f505f0
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-12 16:09:14 -0500

    e1 and e2 complete

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+def double(my_number):
+    final = my_number*2
+    return final
+
+print(double(2))
+print(double(50))
+print(double(27))
---
+++ exercise2.py
@@ -0,0 +1,9 @@
+def negative(arg):
+    if arg != abs(arg):
+        print('True')
+    else:
+        print('False')
+
+negative(-1)
+negative(1)
+negative(-12)
