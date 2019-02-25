6167a6f4ce14072676884f9bad8a59e344d05c41
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-24 20:44:53 -0500

    lint fixes

--- exercise4.py
+++ exercise4.py
@@ -1,11 +1,12 @@
 def charcheck(arg):
-    if type(arg) == type('string'):
+    if isinstance(arg, str):
         if len(arg) <= 8:
             print('False')
         else:
             print('True')
     else:
         print('Not a string, enter a string')
+
 
 charcheck(2)
 charcheck('james')
